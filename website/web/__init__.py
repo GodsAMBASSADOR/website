ae57c8fd4ee7abba8b216f92940ede567f6a532c
GodsAMBASSADOR <135479874+GodsAMBASSADOR@users.noreply.github.com>
2023-11-30 09:14:40 +0100

    Add files via upload

---
+++ website/web/__init__.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'sammas massam'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    return app
+
+
+def create_database(app):
+    # Importing here to avoid circular imports
+    from .models import User, Note
+
+    # Initialize the database with the Flask application context
+    with app.app_context():
+        if not path.exists('instance/' + DB_NAME):
+            # Create all tables defined in the models
+            db.create_all()
+            print('Created Database!')
+            
